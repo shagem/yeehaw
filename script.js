18ecb6c33645e927e5a4603726ebc7b295dda76e
Sean <56557448+shagem@users.noreply.github.com>
2024-01-11 16:46:29 -0500

    0.03a
    added search function, updated image paths, housekeeping / cleaned up code

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const options = {
 };
 
 let fetchedArray;
-
 fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
   .then(response => response.json())
   .then(function(response) {
@@ -16,39 +15,46 @@ fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', option
     })
   .catch(err => console.error(err));
 
+let mainContent;
+let movieCard;
+let movieImg;
+let imgSource;
+let movieTitle;
+let movieOverview;
+let movieRating;
+let ratingColor;
 
-//Generates dynamic movie cards after fetching API data
+//Generates dynamic movie cards after fetching API data on startup
 function generateCards() {
     for (let i = 0; i < fetchedArray.results.length; i++){
-    let mainContent = document.querySelector(".mainContent");
-    let movieCard = document.createElement("div");
+    mainContent = document.querySelector(".mainContent");
+    movieCard = document.createElement("div");
         movieCard.classList = "movie-card";
         mainContent.appendChild(movieCard);
+    //Image
+    movieImg = document.createElement("div");
+        movieImg.id = "movie-img-wrapper";
+    imgSource = document.createElement("img");
+        imgSource.id = "movie-img";
+        imgSource.src = "https://image.tmdb.org/t/p/w342/" + fetchedArray.results[i].poster_path;
+        imgSource.alt = "Sorry, there is no image for this listing";
+        movieCard.appendChild(movieImg);
+        movieImg.appendChild(imgSource);
     //Title
-    let movieTitle = document.createElement("div");
+    movieTitle = document.createElement("div");
         movieTitle.id = "movie-title";
         movieCard.appendChild(movieTitle);
         movieTitle.innerHTML = fetchedArray.results[i].title;
-    //Image
-    let movieImg = document.createElement("div");
-        movieImg.id = "movie-img";
-    let imgSource = document.createElement("img");
-        imgSource.src = "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg";
-        imgSource.alt = "Alt Text For Image";
-        imgSource.height = 400;
-        imgSource.width = 300;
-        movieCard.appendChild(movieImg);
-        movieImg.appendChild(imgSource);
     //Overview
-    let movieBio = document.createElement("div");
-        movieBio.id = "movie-bio";
-        movieCard.appendChild(movieBio);
-        movieBio.innerHTML = fetchedArray.results[i].overview;
+    movieOverview = document.createElement("div");
+        movieOverview.id = "movie-overview";
+        movieCard.appendChild(movieOverview);
+        movieOverview.innerHTML = fetchedArray.results[i].overview;
     //Rating
-    let movieRating = document.createElement("div");
+    movieRating = document.createElement("div");
         movieRating.id = "movie-rating";
         movieCard.appendChild(movieRating);
-    let ratingColor = fetchedArray.results[i].vote_average;
+    ratingColor = fetchedArray.results[i].vote_average;
         ratingColor = Math.round(ratingColor * 10) / 10;
         if (ratingColor <= 5) {
             movieRating.style.color = "var(--vibrantRed)";
@@ -57,7 +63,7 @@ function generateCards() {
         } else {
             movieRating.style.color = "var(--vibrantGreen)";
         }
-        movieRating.innerHTML = ratingColor + " / 10";
+        movieRating.innerHTML = "Average Rating: " + ratingColor + " / 10";
     }
 };
 
@@ -68,3 +74,76 @@ burgerIcon.addEventListener("click", function() {
     document.querySelector("#menu-links").classList.toggle("active");
     burgerIcon.classList.toggle("active");
 });
+
+
+//Search Bar Functionality
+document.querySelector('#search-btn').addEventListener('click', searchMovie);
+document.querySelector("#search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        searchMovie();
+    }
+});
+
+
+//Generates Movie / TV Cards based on Search Bar
+let userSearch;
+function searchMovie() {
+    userSearch = document.querySelector('#search-bar').value;
+    if (userSearch === "") {
+        console.log("Failed Search: No user input...");
+    } else if (userSearch === "sean hageman") {
+        console.log("Lil Easter Egg in the project never hurt anyone...");
+    } else {
+    fetch('https://api.themoviedb.org/3/search/multi?query='+ userSearch +'&include_adult=true&language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(function(response) {
+        fetchedArray = response;
+        console.log(userSearch);
+        console.log(fetchedArray);
+        mainContent.innerHTML = "";
+        for (let i = 0; i < fetchedArray.results.length; i++){
+    mainContent = document.querySelector(".mainContent");
+    movieCard = document.createElement("div");
+        movieCard.classList = "movie-card";
+        mainContent.appendChild(movieCard);
+    //Image
+    movieImg = document.createElement("div");
+        movieImg.id = "movie-img-wrapper";
+    imgSource = document.createElement("img");
+        imgSource.id = "movie-img";
+        imgSource.src = "https://image.tmdb.org/t/p/w342/" + fetchedArray.results[i].poster_path;
+        imgSource.alt = "Sorry, there is no image for this listing";
+        movieCard.appendChild(movieImg);
+        movieImg.appendChild(imgSource);
+    //Title
+    movieTitle = document.createElement("div");
+        movieTitle.id = "movie-title";
+        movieCard.appendChild(movieTitle);
+        movieTitle.innerHTML = fetchedArray.results[i].title;
+    //Overview
+    movieOverview = document.createElement("div");
+        movieOverview.id = "movie-overview";
+        movieCard.appendChild(movieOverview);
+        movieOverview.innerHTML = fetchedArray.results[i].overview;
+    //Rating
+    movieRating = document.createElement("div");
+        movieRating.id = "movie-rating";
+        movieCard.appendChild(movieRating);
+    ratingColor = fetchedArray.results[i].vote_average;
+        ratingColor = Math.round(ratingColor * 10) / 10;
+        if (ratingColor <= 5) {
+            movieRating.style.color = "var(--vibrantRed)";
+        } else if (ratingColor > 5 && ratingColor <= 7) {
+            movieRating.style.color = "var(--vibrantYellow)";
+        } else {
+            movieRating.style.color = "var(--vibrantGreen)";
+        }
+        movieRating.innerHTML = "Average Rating: " + ratingColor + " / 10";
+    }
+    })
+        .catch(err => console.error(err));
+}
+};
+
+
+
